09d6447452f9f5cfc6e3f59526472af83c692c15
shimamatz <shimamatz@gmail.com>
2022-07-18 10:03:40 +0900

    各一覧画面の簡易的表示

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\DrinkAdminContoller; 
+use App\Http\Controllers\DrinkStockContoller;
 
 Route::get('/', function () {
     return view('index');
 });
+Route::get('/', [DrinkStockContoller::class, 'index'])
+    ->name('stock.index');
+
+
+Route::get('/admin', [DrinkAdminContoller::class, 'index'])
+    ->name('admin.index');
